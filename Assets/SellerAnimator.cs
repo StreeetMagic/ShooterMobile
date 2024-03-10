87314eb9bea05e2e4ee9edc802a5a7bbb186d7ca
Валерий Чепик <chepik.valeriy@gmail.com>
2024-03-10 21:21:44 +0300

    mel

---
+++ Assets/SellerAnimator.cs
@@ -0,0 +1,20 @@
+using Gameplay.BaseTriggers;
+using UnityEngine;
+
+public class SellerAnimator : MonoBehaviour
+{
+    [SerializeField] private BaseTrigger _baseTrigger;
+    [SerializeField] private Animator _animator;
+
+    private string _isTalkAnimation = "IsTalk";
+
+    public void PlayTalkAnimation()
+    {
+        _animator.SetBool(_isTalkAnimation, true);
+    }
+
+    public void StopTalkAnimation()
+    {
+        _animator.SetBool(_isTalkAnimation, false);
+    }
+}
