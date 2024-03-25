13f3b854e39b35826303a76ecf4728411013d016
Валерий Чепик <chepik.valeriy@gmail.com>
2024-03-25 14:08:56 +0300

    start_npc_animator_script

---
+++ Assets/_SOURCE/Gameplay/Characters/NPC/StartNPCAnimator.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Gameplay.Characters.NPC
+{
+    public class StartNpcAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        
+        private static readonly int Talk = Animator.StringToHash(IsTalk);
+
+        private const string IsTalk = nameof(IsTalk);
+
+        public void PlayTalkAnimation()
+        {
+            _animator.SetBool(Talk, true);
+        }
+
+        public void StopTalkAnimation()
+        {
+            _animator.SetBool(Talk, false);
+        }
+    }
+}
