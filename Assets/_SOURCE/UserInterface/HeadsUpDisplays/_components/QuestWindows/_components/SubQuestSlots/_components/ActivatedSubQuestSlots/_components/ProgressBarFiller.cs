dcd1a41ba01d33ae0997be2a3796551d7f770d28
Vlad Parastaev <streeetmagic@gmail.com>
2024-04-27 23:34:19 +0400

    Progress bar fixed

--- Assets/_SOURCE/UserInterface/HeadsUpDisplays/_components/QuestWindows/_components/SubQuestSlots/_components/ActivatedSubQuestSlots/_components/ProgressBarFiller.cs
+++ Assets/_SOURCE/UserInterface/HeadsUpDisplays/_components/QuestWindows/_components/SubQuestSlots/_components/ActivatedSubQuestSlots/_components/ProgressBarFiller.cs
@@ -1,15 +1,22 @@
+using System;
 using Configs.Resources.QuestConfigs.Scripts;
 using Quests.Subquests;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace UserInterface.HeadsUpDisplays.QuestWindows._components.SubQuestSlots._components.ActivatedSubQuestSlots._components
 {
   public class ProgressBarFiller : MonoBehaviour
   {
-    public RectTransform RectTransform;
+    public Slider Slider;
 
     [Inject] private SubQuest _subQuest;
     [Inject] private SubQuestSetup _setup;
+
+    private void Update()
+    {
+      Slider.value = (float)_subQuest.CompletedQuantity.Value / _setup.Quantity;
+    }
   }
 }
