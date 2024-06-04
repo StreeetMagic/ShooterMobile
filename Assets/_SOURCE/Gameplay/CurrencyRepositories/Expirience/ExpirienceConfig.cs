cd6420f9f3c72589ac81b87649045da8c7910183
Vlad Parastaev <streeetmagic@gmail.com>
2024-06-05 00:31:43 +0400

    add Simeon configs

--- Assets/_SOURCE/Gameplay/CurrencyRepositories/Expirience/ExpirienceConfig.cs
+++ Assets/_SOURCE/Gameplay/CurrencyRepositories/Expirience/ExpirienceConfig.cs
@@ -6,6 +6,7 @@ namespace CurrencyRepositories.Expirience
   [CreateAssetMenu(menuName = "Configs/ExpirienceConfig", fileName = "ExpirienceConfig")]
   public class ExpirienceConfig : ScriptableObject
   {
+    [Header("Это настройки количества опыта для получения следующего уровня")]
     public List<ExpirienceSetup> Levels;
   }
 }
