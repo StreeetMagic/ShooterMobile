e2a72beb0f8278dfc6f8340a6b98510387b42de5
Vlad Parastaev <streeetmagic@gmail.com>
2024-06-16 20:54:06 +0400

    add final fix

--- Assets/_SOURCE/Infrastructure/VisualEffects/VisualEffectFactory.cs
+++ Assets/_SOURCE/Infrastructure/VisualEffects/VisualEffectFactory.cs
@@ -1,5 +1,4 @@
 using System;
-using Infrastructure.AssetProviders;
 using Infrastructure.ZenjectFactories.SceneContext;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -8,13 +7,11 @@ namespace Infrastructure.VisualEffects
 {
   public class VisualEffectFactory
   {
-    private readonly AssetProvider _assetProvider;
     private readonly VisualEffectService _visualEffectService;
     private readonly GameLoopZenjectFactory _zenjectFactory;
 
-    public VisualEffectFactory(AssetProvider assetProvider, GameLoopZenjectFactory zenjectFactory, VisualEffectService visualEffectService)
+    public VisualEffectFactory(GameLoopZenjectFactory zenjectFactory, VisualEffectService visualEffectService)
     {
-      _assetProvider = assetProvider;
       _zenjectFactory = zenjectFactory;
       _visualEffectService = visualEffectService;
     }
@@ -23,9 +20,8 @@ namespace Infrastructure.VisualEffects
     {
       ParticleSystem prefab = _visualEffectService.GetPrefab(visualEffectId);
       GameObject instance = _zenjectFactory.InstantiateGameObject(prefab.gameObject, position, Quaternion.identity, parent);
-     // instance.transform.SetParent(null);
-      float duration1 = prefab.main.duration;
-      Object.Destroy(instance, duration1);
+      float duration = prefab.main.duration;
+      Object.Destroy(instance, duration);
     }
   }
 }
