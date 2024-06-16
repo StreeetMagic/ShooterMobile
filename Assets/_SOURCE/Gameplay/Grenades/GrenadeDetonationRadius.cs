17200f3e9f5ed76e52152a2242a9e936662677f9
Валерий Чепик <chepik.valeriy@gmail.com>
2024-06-16 20:16:52 +0400

    grenade vfx and ui radius

--- Assets/_SOURCE/Gameplay/Grenades/GrenadeDetonationRadius.cs
+++ Assets/_SOURCE/Gameplay/Grenades/GrenadeDetonationRadius.cs
@@ -1,15 +1,27 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Gameplay.Grenades
 {
   public class GrenadeDetonationRadius : MonoBehaviour
   {
+    [SerializeField] private RectTransform _detonationRadius;
+    
     public void Init(GrenadeConfig config)
     {
       float radius = config.DetonationRadius;
       float localRaius = radius * 2;
 
       transform.localScale = new Vector3(localRaius, localRaius, localRaius);
+      
+      StartDetonationDuration(radius, config.DetonationTime);
     }
+
+    private void StartDetonationDuration(float targetScale, float duration)
+    {
+      _detonationRadius.DOScale(new Vector3(targetScale, targetScale, targetScale), duration)
+          .SetEase(Ease.InOutQuad);
+    }
+    
   }
 }
